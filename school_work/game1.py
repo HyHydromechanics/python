5f67e8ae28dea6ea43b37ef73b37e634097e8fa2
Hydromechanics <15075152129@163.com>
2022-04-21 10:00:49 +0800

    a

--- school_work/game1.py
+++ school_work/game1.py
@@ -1,13 +1,63 @@
 import tkinter as tk
 from tkinter import *
-import tkinter.messagebox
-
-x = 1
 
 
+nameList = [0, 0, 0]
+valueList = [0, 0, 0]
+
+nl = iter(nameList)
+vl = iter(valueList)
+
+rows = 3
+columns = 3
 
 windows = tk.Tk()
 windows.title("Game")
-windows.geometry("500x500")
+
+cell1 = tk.Text(windows, width=10, height=5)
+cell1.grid()
+c1 = cell1.getint()
+
+cell2 = tk.Text(windows, width=10, height=5)
+cell2.grid()
+c2 = cell2.getint()
+
+cell3 = tk.Text(windows, width=10, height=5)
+cell3.grid()
+c3 = cell3.getint()
+
+cell4 = tk.Text(windows, width=10, height=5)
+cell4.grid()
+c4 = cell4.getint()
+
+cell5 = tk.Text(windows, width=10, height=5)
+cell5.grid()
+c5 = cell5.getint()
+
+cell6 = tk.Text(windows, width=10, height=5)
+cell6.grid()
+c6 = cell6.getint()
+
+cell7 = tk.Text(windows, width=10, height=5)
+cell7.grid()
+c7 = cell7.getint()
+
+cell8 = tk.Text(windows, width=10, height=5)
+cell8.grid()
+c8 = cell8.getint()
+
+cell9 = tk.Text(windows, width=10, height=5)
+cell9.grid()
+c9 = cell9.getint()
+
+if c1 + c5 + c9 == 3:
+    tk.messagebox.showinfo("Player 1 Won!")
+elif c1 + c5 + c9 == 6:
+    tk.messagebox.showinfo("Player 2 Won!")
+
+if c3 + c5 + c7 == 3:
+    tk.messagebox.showinfo("Player 1 Won!")
+elif c1 + c2 + c3 == 6:
+    tk.messagebox.showinfo("Player 2 Won!")
 
 windows.mainloop()
---
+++ school_work/tkintertable.py
@@ -0,0 +1,18 @@
+import tkinter
+from tkintertable import TableCanvas, TableModel
+from tkinter import *
+
+master = tkinter.Tk()
+master.geometry('400x500')
+
+tframe = Frame(master)
+tframe.pack()
+
+data = {'rec1': {'col1': 99.88, 'col2': 108.79, 'label': 'rec1'},
+       'rec2': {'col1': 99.88, 'col2': 108.79, 'label': 'rec2'}
+       } 
+
+table = TableCanvas(tframe, data=data)
+table.show()
+
+master.mainloop()
